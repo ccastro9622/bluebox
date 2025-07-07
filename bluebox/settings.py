42bfca7954da8fab4b96eb316844c08d0f1760f9
ccastro9622 <cristiano.castro@pcline.com.br>
2025-07-07 16:48:34 -0300

    avaliacao

--- bluebox/settings.py
+++ bluebox/settings.py
@@ -158,11 +158,13 @@ STATICFILES_DIRS = ['static']
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST = 'smtp.office365.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = "suporte@bluebox21.com"
+# EMAIL_HOST_PASSWORD = "Drom3D@lho"
 EMAIL_HOST_USER = "pclinecomputadores@gmail.com"
 EMAIL_HOST_PASSWORD = "vskw xzlz roio ruuq"
-
 
 
 
