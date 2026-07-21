14dbf9575267cc1db19f53e8457bc45504c2d9b0
ccastro9622 <cristiano.castro@pcline.com.br>
2026-07-21 14:11:37 -0300

    make it better

--- bluebox/settings.py
+++ bluebox/settings.py
@@ -175,15 +175,15 @@ STATIC_URL = '/static/'
 AUTH_USER_MODEL = 'user_account.CustomUser'
 STATICFILES_DIRS = ['static']
 
-# Configurações de envio do e-mail
+# Configurações de envio do e-mail pela bluebox
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'mail.bluebox21.com.br'
-EMAIL_PORT = 465
+EMAIL_PORT = 587
 EMAIL_USE_SSL = False
-EMAIL_USE_TLS = False
+EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "contato@bluebox21.com.br"
 EMAIL_HOST_PASSWORD = "AnChCr2021!"
-
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_HOST = 'smtp.gmail.com'
